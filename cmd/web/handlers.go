940c64c6fcdeb759ab43160bbd88847977b966d5
zanderson3j <zanderson3j@gmail.com>
2020-10-06 23:00:20 -0400

    chapter 5

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,8 +3,6 @@ package main
 import (
 	"errors"
 	"fmt"
-
-	// "html/template"
 	"net/http"
 	"strconv"
 
@@ -23,26 +21,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
-
-	// files := []string{
-	//     "./ui/html/home.page.tmpl",
-	//     "./ui/html/base.layout.tmpl",
-	//     "./ui/html/footer.partial.tmpl",
-	// }
-
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	//     app.serverError(w, err)
-	//     return
-	// }
-
-	// err = ts.Execute(w, nil)
-	// if err != nil {
-	//     app.serverError(w, err)
-	// }
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
@@ -62,7 +43,9 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "%v", s)
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
 }
 
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
